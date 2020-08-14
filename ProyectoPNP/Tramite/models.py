0d25885e6a19865d840fddbbe8d97fb3dc77f1f6
Gonzalo Caira <gc98wootz@gmail.com>
2020-08-14 01:53:29 +0000

    Revert "Merge branch 'Jaime' into 'master'"
    This reverts merge request !14

--- ProyectoPNP/Tramite/models.py
+++ ProyectoPNP/Tramite/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import model_to_dict
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -151,19 +152,6 @@ class Documentos(models.Model):
     estado = models.IntegerField()
     tipo = models.IntegerField()
     observacion = models.TextField()
-
-    def serializeCustom(self):
-        data = {
-            "numero_doc": self.n_documento,
-            "hoja": self.numero_Hoja,
-            "unidad_entrega": self.unidadEntrega,
-            "quien_entrega": self.quien_entrega,
-            "unidad_Destino": self.unidad_Destino,
-            "fecha_documento": self.fecha_documento,
-            "area_destino": self.area_destino,
-            "encargado": self.encargado,
-        }
-        return data
 
     class Meta:
         verbose_name_plural = "Documentos"
